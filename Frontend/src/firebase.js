09f303740249a4eca01b80b32c1c4a31162d5a35
Narsale Shreyas Manohar <shreyasnarsale04@gmail.com>
2026-02-12 16:50:04 +0530

    Update firebase.js

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -11,5 +11,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+console.log("FIREBASE CONFIG:", firebaseConfig);
+
 export const auth = getAuth(app);
 export default app;
